ab99c9d23c3a6ec7ceea1e2da30708bb89870aaa
Nerdenator <a.mcruer@live.com>
2021-11-09 22:15:14 -0600

    trying to figure out if there's a max compression for given files.

--- main.py
+++ main.py
@@ -1,36 +1,53 @@
-# This is a sample Python script.
-
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import getopt
-import sys
+import argparse
+from io import StringIO, BytesIO
+from PIL import Image
 
 
-def compressor(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def compressor(input_file, output_file, compression_cycles):
+
+    # https://stackoverflow.com/questions/30771652/how-to-perform-jpeg-compression-in-python-without-writing-reading
+    buffer = BytesIO()
+    img = Image.open(input_file)
+    img.save(fp=buffer, format="JPEG", quality=100)
+    img.show()
+
+    buffer.seek(0)
+    for i in range(compression_cycles):
+        imagefrombuffer = Image.open(buffer)
+        buffer2 = BytesIO()
+        imagefrombuffer.save(fp=buffer2, format="JPEG", quality=90)
+        buffer.truncate(0)
+        buffer = buffer2
+
+    with open(output_file, "wb") as out:
+        buffer.seek(0)
+        out.write(buffer.read())
+    imag = Image.open(output_file)
+    imag.show()
 
 
-# Press the green button in the gutter to run the script.
+
+    # buffer = BytesIO()
+    # img.save(buffer, "JPEG")    # https://jdhao.github.io/2019/07/20/pil_jpeg_image_quality/, using 75 as default
+    #
+    # # buffer.seek(0)
+    # with open(output_file, "wb") as handle:
+    #     handle.write(buffer.getbuffer())
+
+
+
+
 if __name__ == '__main__':
-    input_file = ''
-    output_file = ''
-    compression_cycles = 0
-    try:
-        opts, args = getopt.getopt("hi:o:c:",["ifile=","ofile","compressioncycles"])
-    except getopt.GetoptError as ex:
-        print(str(ex))
-        print('moar_compression.py -i <inputfile> -o <outputfile> -c <compressioncycles>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('moar_compression.py -i <inputfile> -o <outputfile> -c <compressioncycles>')
-        elif opt in("-i", "--ifile"):
-            input_file = arg
-        elif opt in ("-o", "--ofile"):
-            output_file = arg
-        elif opt in ("-c", "--cycles"):
-            compression_cycles = arg
-    compressor(input_file, output_file, compression_cycles)
+    parser = argparse.ArgumentParser()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    parser.add_argument('-o', '--output', help='Output file name')
+    parser.add_argument('-i', '--input', help='Input file name')
+    parser.add_argument('-c', '--cycles', help='Compression cycles')
+
+    args = parser.parse_args()
+    input = args.input
+    output = args.output
+    cycles = int(args.cycles)
+    compressor(input, output, cycles)
+
+
